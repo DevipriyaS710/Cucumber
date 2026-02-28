a5a13c7338cf0d59c9a19b80fc35f6b8eed5ec52
DevipriyaS710 <rdevipriya94@gmail.com>
2026-02-28 21:52:17 +0530

    init commit

--- Feb_Cucumber/src/test/java/com/app/runner/RunnerLogin.java
+++ Feb_Cucumber/src/test/java/com/app/runner/RunnerLogin.java
@@ -10,7 +10,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "C:\\Users\\Admin\\Downloads\\Feb_Cucumber\\Feb_Cucumber\\src\\test\\java\\com\\app\\feature\\LoginPage.feature",
                  glue = "com.app.stepdefinition",
-                 dryRun = false,
+                 dryRun = false
                  
     
                  )
